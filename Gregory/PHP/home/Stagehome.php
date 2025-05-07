75834a338c472b49a112c3906be3d859de920d57
Grégory Gilliéron <gregory.gillieron@epfl.ch>
2025-05-07 11:17:24 +0200

    fix

--- Gregory/PHP/home/Stagehome.php
+++ Gregory/PHP/home/Stagehome.php
@@ -28,7 +28,7 @@ if (!isset($_SESSION["user_id"])) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>EPFL - Home</title>
-    <link rel="stylesheet" href="../CSS/HOME/Home.css">
+    <link rel="stylesheet" href="../../CSS/HOME/Home.css">
     
 </head>
 <body>
